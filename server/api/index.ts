cd06d6d559f2fb2be3f9de55c762c089d06a05f3
CamlineKe <camlinemoses01@gmail.com>
2026-01-17 17:14:45 +0300

    added some production changes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,2 +1,10 @@
 import app from '../src/app';
-export default app;
+import connectDB from '../src/config/database';
+
+export default async function handler(req: any, res: any) {
+    // Connect to the database
+    await connectDB();
+
+    // Hand off the request to the Express application
+    return app(req, res);
+}
